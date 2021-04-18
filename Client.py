02d9cd55cf9fa0e673b190ae79d13c4ba8c666ad
Andris Andris <zuper1998@gmail.com>
2021-04-18 12:55:49 +0200

    Done with enumarations and sceleton for the commands

--- Client.py
+++ Client.py
@@ -42,7 +42,3 @@ class Client():
         print(message)
         return message
 
-
-def byte_xor(ba1, ba2):
-    """ XOR two byte strings """
-    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
